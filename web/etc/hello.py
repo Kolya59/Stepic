2eedd839d70ff7fa4387cffb77ad09be66adc233
Kolya59 <kolya.nikitas@yandex.ru>
2018-12-25 21:30:37 +0500

    Попытка поменять местами файлы

--- web/etc/hello.py
+++ web/etc/hello.py
@@ -1,5 +1,11 @@
-CONFIG = {
-	'mode': 'wsgi'
-	'working_dir' : 'home/box/web/'
-	'args': 'hello:wsgi_application'
-}
+def wsgi_application(environ, start_response):
+	query_str = environ['QUERY_STRING']
+	body = [
+		bytes(i + '\n', 'ascii') for i in query_str.split('&')
+	]
+	status = '200'
+	headers = [
+		('Content-Type', 'text/plain')
+	]
+	start_response(status, headers)
+	return [ body ]
--- web/hello.py
+++ web/hello.py
@@ -1,11 +1,5 @@
-def wsgi_application(environ, start_response):
-	query_str = environ['QUERY_STRING']
-	body = [
-		bytes(i + '\n', 'ascii') for i in query_str.split('&')
-	]
-	status = '200'
-	headers = [
-		('Content-Type', 'text/plain')
-	]
-	start_response(status, headers)
-	return [ body ]
+CONFIG = {
+	'mode': 'wsgi'
+	'working_dir' : 'home/box/web/etc/'
+	'args': 'hello:wsgi_application'
+}
