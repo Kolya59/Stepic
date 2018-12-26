c5e6b27bc30fd690f3c2c021f4e537a6c4ee73b4
Kolya59 <kolya.nikitas@yandex.ru>
2018-12-27 01:54:00 +0500

    Another try

---
+++ ask/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/kolya59/Yandex.Disk.localized/web/web/ask/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ hello.py
@@ -0,0 +1,11 @@
+def wsgi_application(environ, start_response):
+    query_str = environ['QUERY_STRING']
+    body = [
+        bytes(i + '\n', 'ascii') for i in query_str.split('&')
+    ]
+    status = '200'
+    headers = [
+        ('Content-Type', 'text/plain')
+    ]
+    start_response(status, headers)
+    return [body]
