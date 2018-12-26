4c3c16d9a148bb9a6eac46b43ac3af905ecba813
Kolya59 <kolya.nikitas@yandex.ru>
2018-12-27 01:12:28 +0500

    Another try

--- web/hello.py
+++ web/hello.py
@@ -1,5 +1,10 @@
 CONFIG = {
-	'mode': 'wsgi'
-	'working_dir' : 'home/box/web/etc/'
-	'args': 'hello:wsgi_application'
+	# 'mode': 'wsgi',
+	'working_dir' : 'home/box/web/etc/',
+	'args': (
+		# '--bind=127.0.0.1:8080',
+		'--workers=16'
+		'--timeout=60'
+		'hello.wsgi_application'
+	),
 }
